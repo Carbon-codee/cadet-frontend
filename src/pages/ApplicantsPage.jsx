ec53c2c3f148217951c3081352fb4f9d380c49a0
Şaban Doğan <saban.dogan714@gmail.com>
2026-01-19 00:19:22 +0300

    Fix import path for axiosConfig

--- src/pages/ApplicantsPage.jsx
+++ src/pages/ApplicantsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import API from '../axiosConfig';
+import API from '../api/axiosConfig';
 import './ApplicantsPage.css';
 
 const ApplicantsPage = () => {
